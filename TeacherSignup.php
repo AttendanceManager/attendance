bbaa324ab82064c5a1a3aaa697e9fbf5c649e672
amish gupta <amishgupta66@gmail.com>
2017-07-19 00:29:05 +0530

    mail functionality added in TeacherSignup.php

--- TeacherSignup.php
+++ TeacherSignup.php
@@ -17,6 +17,7 @@
     
     
 <?php
+
     require 'db.php';
     if ($_SERVER["REQUEST_METHOD"] == "POST"  && !empty($_POST['Submit']) && isset($_POST['Submit']))
     { 
@@ -53,6 +54,27 @@
             $query2 = "INSERT INTO TeacherBase (TId, Email, FirstName, MiddleName, LastName) VALUES ('$TId', '$Email', '$FName','$MName','$LName')";
             
            // mail($Email,"JMI Attendance Manager","Your password is : ".$Pass);
+		   //mail code -: amish 18/7
+		    
+if(isset($_POST['Submit'])){
+    $to = $_POST['Email']; // this is your Email address
+    
+    $first_name = $_POST['FName'];
+	$mid_name=$_POST['MName'];
+	$last_name=$_POST['LName'];
+    $subject = "Password for Attendance Manager";
+ 
+    $message = "Name:".$first_name ."\n" ."Middle Name:".$mid_name."\n"."Last Name:".$last_name."\n"."Your Password:".$Pass;
+    
+
+   
+    $headers = "From Attendance Manager JMI";
+    mail($to,$subject,$message,$headers);
+     
+    echo "Mail Sent. Thank you " . $first_name . ", has been send a mail containing password.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+	//mail code ends -:amish
             $flag = 0;
         
             if(!$conn->query($query1))
